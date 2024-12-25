3502ec58cc534046065e9b499b6e4f052cc5a687
natemvm <84608411+natemvm@users.noreply.github.com>
2024-12-24 19:43:48 -0500

    changd icons

--- react-app/vite-project/electron/electron.js
+++ react-app/vite-project/electron/electron.js
@@ -10,7 +10,7 @@ function createWindow() {
     autoHideMenuBar: true,
     resizable: false,
     frame: true,
-    icon: "icon.ico",
+    icon: "ic_launcher_round.png",
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
